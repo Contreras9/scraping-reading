868569ba620e3fbb87c3ad4f27453c53d30ef00a
Hamzah Contreras <Hamzah_36@outlook.com>
2022-06-17 21:18:21 -0500

    Coded along

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# puts doc.css(".heading-financier").text.strip
+
+# courses = doc.css(".color-black")
+
+p doc.css(".color-black")[0].attributes
+
+# courses.each do |course|
+#    puts course.text.strip
+# end
